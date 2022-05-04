cb5aef8674e737caa595e631786fd86fa36102e3
mintain <3209304627@qq.com>
2022-05-04 22:52:46 +0800

    字符串

---
+++ gktime_getting_started/README.md
@@ -0,0 +1,3 @@
+# 极客时间 Go 语言从入门到实战
+
+课程链接 [https://time.geekbang.org/course/intro/100024001](https://time.geekbang.org/course/intro/100024001)
---
+++ gktime_getting_started/chapter_21_test.go
@@ -0,0 +1,46 @@
+﻿package gktime_getting_started
+
+import (
+	"reflect"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+type Test struct {
+}
+
+func TestStringType(t *testing.T) {
+	pointer := &Test{}
+	t.Log(reflect.TypeOf(pointer))
+	str := "str1"
+	t.Log(reflect.TypeOf(str))
+	t.Log(len(str))
+	s := "\xe4\xb8\xA5"
+	t.Log(s)
+	s = "中"
+	t.Log(len(s))
+	c := []rune(s)
+	t.Log(len(c))
+	t.Logf("中 Unicode %x", c[0])
+	t.Logf("中 UTF8 %x", s)
+}
+
+func TestStringToRune(t *testing.T) {
+	s := "测试中...𠮷𠮷𠮷"
+	for _, c := range s {
+		t.Logf("%[1]c %[1]x", c)
+	}
+}
+
+func TestStringFun(t *testing.T) {
+	str := "A,B,C"
+	strSlice := strings.Split(str, ",")
+	t.Log(strSlice)
+	t.Log(strings.Join(strSlice, "_"))
+	s := strconv.Itoa(10)
+	t.Log("s" + s)
+	if num, err := strconv.Atoi("10"); err == nil {
+		t.Log(10 + num)
+	}
+}
