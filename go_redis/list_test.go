f7ac92354c50acfa046ab19e9cbd872211e78f98
mintain <3209304627@qq.com>
2022-05-13 13:17:11 +0800

    list BLMove

--- go_redis/list_test.go
+++ go_redis/list_test.go
@@ -1,6 +1,37 @@
 package go_redis
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
+
+// 当List 里面有数据时候 BLMove 和 LMove 操作效果一样，当list里面没有数据的时候BLMove 会阻塞住当前代码运行，直到队列里面有数据。
+// 其他操作和 LMove 相同，具体可以参考 TestLMove 函数
+func TestBLMove(t *testing.T) {
+	key := "test:BLMove:k1"
+	key2 := "test:BLMove:k2"
+	defer func() {
+		client.Del(ctx, key, key2)
+	}()
+	client.RPush(ctx, key, "one")
+	client.RPush(ctx, key, "two")
+	client.RPush(ctx, key, "three")
+	res := client.BLMove(ctx, key, key2, "RIGHT", "LEFT", 0)
+	t.Log(res)
+	res = client.BLMove(ctx, key, key2, "LEFT", "RIGHT", 0)
+	t.Log(res)
+	res = client.BLMove(ctx, key, key2, "LEFT", "RIGHT", 0)
+	t.Log(res)
+	// 超时失败
+	res = client.BLMove(ctx, key, key2, "LEFT", "RIGHT", time.Second*5)
+	t.Log(res)
+	go func() {
+		time.Sleep(time.Second * 2)
+		client.RPush(ctx, key, "four")
+	}()
+	res = client.BLMove(ctx, key, key2, "LEFT", "RIGHT", 0)
+	t.Log(res)
+}
 
 /**
 Available since: 6.2.0
