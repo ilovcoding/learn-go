53b1371abf3d7fd25265706519c2346f23ac1cd6
mintain <3209304627@qq.com>
2022-05-15 20:51:02 +0800

    list LINSERT

--- go_redis/list_test.go
+++ go_redis/list_test.go
@@ -96,6 +96,13 @@ func TestLIndex(t *testing.T) {
 	t.Log(res)
 }
 
+func TestLInsert(t *testing.T) {
+	key := "test:LInsert:test"
+	client.RPush(ctx, key, "Hello", "World")
+	client.LInsert(ctx, key, "BEFORE", "World", "Three")
+	t.Log(client.LRange(ctx, key, 0, -1))
+}
+
 //  LPUSH mylist "one" "two" "three" "four" "five"
 // LPUSH mylist2 "a" "b" "c" "d" "e"
 // LMPOP 2 mylist mylist2  left count  10 从 两个 list 的 左边出栈10个元素，优先从 mylist 中选，
