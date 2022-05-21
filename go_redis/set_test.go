8b7c50746de552505a78559cc31c34c010e1ed6c
mintain <3209304627@qq.com>
2022-05-21 21:32:16 +0800

    set SInter SInterStore

--- go_redis/set_test.go
+++ go_redis/set_test.go
@@ -11,3 +11,53 @@ func TestSAdd(t *testing.T) {
 	res := client.SMembers(ctx, setKey)
 	t.Log(res)
 }
+
+// 返回 Set 中含有元素个数
+func TestSCard(t *testing.T) {
+	res := client.SCard(ctx, setKey)
+	t.Log(res)
+}
+
+func TestSDiff(t *testing.T) {
+	defer func() {
+		client.Del(ctx, "key1", "key2", "key3")
+	}()
+	client.SAdd(ctx, "key1", "a", "b", "c", "d")
+	client.SAdd(ctx, "key2", "c")
+	client.SAdd(ctx, "key3", "a", "c", "e")
+	res := client.SDiff(ctx, "key1", "key2", "key3")
+	t.Log(res)
+}
+
+// SDIFFSTORE destination key [key ...]
+func TestSDiffStore(t *testing.T) {
+	defer func() {
+		client.Del(ctx, "key1", "key2", "key")
+	}()
+	client.SAdd(ctx, "key1", "a", "b", "c")
+	client.SAdd(ctx, "key2", "c", "d", "e")
+	client.SDiffStore(ctx, "key", "key1", "key2")
+	res := client.SMembers(ctx, "key")
+	t.Log(res)
+}
+
+// 求集合交集 O(M*N)
+func TestSInter(t *testing.T) {
+	defer func() {
+		client.Del(ctx, "key1", "key2", "key3")
+	}()
+	client.SAdd(ctx, "key1", "a", "b", "c", "d")
+	client.SAdd(ctx, "key2", "c")
+	client.SAdd(ctx, "key3", "a", "c", "e")
+	res := client.SInter(ctx, "key1", "key2", "key3")
+	t.Log(res)
+}
+
+func TestSInterStore(t *testing.T) {
+	client.SAdd(ctx, "key1", "a", "b", "c")
+	client.SAdd(ctx, "key2", "c", "d", "e")
+	res := client.SInterStore(ctx, key, "key1", "key2")
+	t.Log(res)
+	res2 := client.SMembers(ctx, key)
+	t.Log(res2)
+}
